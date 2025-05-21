faf4248e40ce61c07739d3d970e5caa9ec7761f8
gmirilla <gmirilla@gmail.com>
2025-05-21 14:29:51 +0100

    begin work on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,16 +10,19 @@
                 @endphp
 <x-layouts.app :title="__('Dashboard')">
     <div class="row g-4">
-        <div class="col-lg-4">
+      @if ($usercheck->role=='agent')
+      <div class="col-lg-4">
           <div class="card">            
             <div class="card-body">
               <div style="font-size: 30px"><i class="fa fa-credit-card" aria-hidden="true"></i></div>
               <h4 class="text-center">{{$creditleft}}</h4></div>
             <div class="card-footer"><h4 class="text-center">Upload Credit(s) Left</h4></div>
           </div>
-
-
         </div>
+      @endif    
+
+
+        
         <div class="col-lg-4">
           <div class="overflow-hidden rounded border" style="aspect-ratio: 16/6;">
             <x-placeholder-pattern class="h-100 w-100" style="stroke: color-mix(in oklab, oklch(.21 .034 264.665) 20%, transparent);" />
